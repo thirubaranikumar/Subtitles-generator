ff7fbad22a9e3a6884338ff493934a44f09a0839
Barani kumar <baranikumar.unique@gmail.com>
2020-06-13 01:31:13 +0530

    Add files via upload
    Added some code to write subtitle text in srt time format.

--- recognize.py
+++ recognize.py
@@ -6,7 +6,11 @@ import librosa
 import argparse
 import os
 from glob import glob
+from datetime import datetime
+from datetime import timedelta
 
+InitTime = datetime.strptime("00:00:00,000","%H:%M:%S,%f")
+InitCount = 1
 def get_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument('-video', type=str,
@@ -27,26 +31,36 @@ def recognize(wav_filename):
     print('audiofile loaded')
 
     try:
-        result = r.recognize_google(audio, language = 'ru').lower()
+        result = r.recognize_google(audio, language = 'en-IN').lower()
     except sr.UnknownValueError:
         print("cannot understand audio")
         result = ''
         os.remove(wav_filename)  
-    with open('result.txt', 'a', encoding='utf-8') as f:
-        f.write(' {}'.format(result))
+    with open('result_en.txt', 'a', encoding='utf-8') as f:
+        global InitTime
+        global InitCount
+        f.write(str(InitCount)+'\n')
+        f.write(datetime.strftime(InitTime, "%H:%M:%S,%f"))
+        f.write(" --> ")
+        f.write(datetime.strftime(InitTime + timedelta(seconds=25),"%H:%M:%S,%f"))
+        f.write('\n')
+        f.write(' {}'.format(result+'\n'))
+        f.write('\n')
+        InitTime = InitTime + timedelta(seconds=25)
+        InitCount = InitCount + 1
    #  return result
 
 def get_audio(video):
-    os.system('ffmpeg -y  -threads 4\
+    os.system('/Users/barani/Downloads/Applications/ffmpeg -y  -threads 4\
  -i {} -f wav -ab 192000 -vn {}'.format(video, 'current.wav'))
 
 def split_into_frames(audiofile):
     data, sr = librosa.load(audiofile)
     duration = librosa.get_duration(data, sr)
     print('video duration, hours: {}'.format(duration/3600))
-    for i in range(0,int(duration-1),50):
-        tmp_batch = data[(i)*sr:sr*(i+50)]
-        librosa.output.write_wav('samples/{}.wav'.format(chr(int(i/50)+65)), tmp_batch, sr)
+    for i in range(0,int(duration-1),25):
+        tmp_batch = data[(i)*sr:sr*(i+25)]
+        librosa.output.write_wav('samples/{}.wav'.format(chr(int(i/25)+65)), tmp_batch, sr)
 
 if __name__ == '__main__':
     start = time.time()
@@ -55,7 +69,7 @@ if __name__ == '__main__':
     split_into_frames('current.wav')
     files = sorted(glob('samples/*.wav'))
     print(files)
-    open('result.txt', 'w', encoding = 'utf-8').write('')
+    open('result_en.txt', 'w', encoding ='utf-8').write('')
     for file in files:
         print(file)   
         recognize(file)
